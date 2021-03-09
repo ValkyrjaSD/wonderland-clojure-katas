e2628646d169f259f5e6eaa4bdbf56436eb2ae81
saraincin <isara22@ciencias.unam.mx>
2021-03-09 17:08:45 -0600

    Solved magic-square

--- magic-square/project.clj
+++ magic-square/project.clj
@@ -5,3 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :main magic-square.puzzle)
+
+
--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -1,8 +1,64 @@
 (ns magic-square.puzzle)
 
-(def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
+(def values
+  [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
-(defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+(defn- mete [v l]
+       (if (empty? l)
+         (conj [] v)
+         (loop [n l
+                res []]
+               (if (empty? n)
+                 res
+                 (if (number? (first n))
+                   (conj res (conj n v))
+                   (recur (rest n) (conj res (mete v (first n)))))))))
+
+(defn- drop-nth [n coll]
+       (concat
+         (take n coll)
+         (drop (inc n) coll)))
+
+(defn- permutations [values]
+       (loop [n 0
+              per []]
+             (if (= n (count values))
+               per
+               (let [x values]
+                    (recur (inc n) (conj per (mete (nth values n) (permutations (drop-nth n x)))))))))
+
+(defn- sum
+       [coll]
+       (reduce + coll))
+
+(defn- sum-rows [m]
+      (map #(reduce + %) m))
+
+(defn- sum-cols [m]
+      [(reduce + (map first m))
+       (reduce + (map second m))
+       (reduce + (map last m))])
+
+(defn- sum-diagonals
+       [l]
+       (let [left-to-right-diagonals [(first (first l)) (second (second l)) (last (last l))]
+             right-to-left-diagonals [(first (last l)) (second (second l)) (last (first l))]]
+            [(sum left-to-right-diagonals) (sum right-to-left-diagonals)]))
+
+(defn- es-valido?
+       [l]
+       (= (set (sum-rows l))
+          (set (sum-cols l))
+          (set (sum-diagonals l))))
+
+
+(defn magic-square
+      [coll]
+      (->> (permutations coll)
+           flatten
+           (partition 9)
+           (map #(map vec (partition 3 %)))
+           (filter #(es-valido? %))
+           first
+           vec))
+
